4c7a82d0b16242673f602ca393ef26a0d6eb3bb1
XUHO3 <howells.xu@oocl.com>
2018-07-24 16:53:56 +0800

    port

--- src/main/java/com/example/demo/EmployeesController.java
+++ src/main/java/com/example/demo/EmployeesController.java
@@ -13,11 +13,11 @@ public class EmployeesController {
     List<Employees> list=new LinkedList<>();
 
     public EmployeesController (){
-        Employees employees=new Employees("0","小明",20,"男");
-        Employees employees1=new Employees("1","小红",19,"女");
-        Employees employees2=new Employees("2","小智",15,"男");
-        Employees employees3=new Employees("3","小刚",16,"男");
-        Employees employees4=new Employees("4","小霞",15,"女");
+        Employees employees=new Employees("1","小明",20,"男");
+        Employees employees1=new Employees("2","小红",19,"女");
+        Employees employees2=new Employees("3","小智",15,"男");
+        Employees employees3=new Employees("4","小刚",16,"男");
+        Employees employees4=new Employees("5","小霞",15,"女");
         list.add(employees);
         list.add(employees1);
         list.add(employees2);
